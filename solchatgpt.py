7e1de071d4517799e2c84bd256c3ce2f40e8ac15
jsturgisSol <103606721+jsturgisSol@users.noreply.github.com>
2023-04-17 14:59:46 -0400

    Update solchatgpt.py

--- solchatgpt.py
+++ solchatgpt.py
@@ -1,12 +1,13 @@
-import os
 import requests
 import boto3
 import json
+import os
+import re
 
 # API endpoint URL
 endpoint_url = "https://api.openai.com/v1/chat/completions"
 
-# set openai model
+# Set openai model
 model = os.environ['OAI_MODEL']
 
 # JSON request body format
@@ -19,12 +20,20 @@ request_body = {
 
 # Function to handle incoming events
 def lambda_handler(event, context):
+    
     # Extract key1 from the event
     key1 = event['body-json']['key1']
-    #key1 = {
+    
+    #key1_dict = {
     #  "role": "user",
-    #  "content": "create a soql statement to pull all records from the lead table"}
-    #
+    #  "content": "save this thread as test1"}
+    
+    #key1 = key1_dict['content']
+    
+    
+    # Ensure key1 is a string
+    if not isinstance(key1, str):
+        key1 = str(key1)
     
     # Connect to S3
     s3 = boto3.client('s3')
@@ -36,61 +45,65 @@ def lambda_handler(event, context):
     except:
         history = []
 
-    if key1 == "end thread":
-        # Connect to S3
-        s3 = boto3.client('s3')
-        
+    save_thread_pattern = re.compile(r'^save this thread as (.+)$', re.IGNORECASE)
+    save_thread_match = save_thread_pattern.match(key1)
+
+    if key1.lower() == "end thread":
         # Delete the conversation history from S3
         s3.delete_object(Bucket=os.environ['S3_BUCKET_NAME'], Key='conversation_history.json')
         
-        return {
-    "conversation_history": history,
-    "response": {
-        "id": "",
-        "object": "",
-        "created": "",
-        "model": "",
-        "usage": {
-            "prompt_tokens": 0,
-            "completion_tokens": 0,
-            "total_tokens": 0
-        },
-        "choices": [
-            {
-                "message": {
-                    "role": "assistant",
-                    "content": "Thank you, this thread has been terminated"
-                },
-                "finish_reason": "",
-                "index": 0
-            }
-        ]
-    }
-}
-    
-    # Add the new message to the history
-    history.append({"role": "user", "content": key1})
-    
-    # Update the request body with the conversation history
-    request_body["messages"] = history
-    
-    # Send the request to the API endpoint
-    headers = {
-        'Authorization': 'Bearer ' + os.environ['API_KEY']
-    }
-    response = requests.post(endpoint_url, json=request_body, headers=headers)
-    #print(response)
-    #print(request_body)
-    # Add the response from the API to the conversation history
-    response_json = response.json()
-    for choice in response_json["choices"]:
-        history.append(choice["message"])
-    
-    # Store the updated history in S3
-    s3.put_object(Bucket=os.environ['S3_BUCKET_NAME'], Key='conversation_history.json', Body=json.dumps(history))
-    
-    # Return the response from the API and the conversation history
+        response_text = "Thank you, this thread has been terminated"
+    elif save_thread_match:
+        filename = save_thread_match.group(1).strip() + '.json'
+
+        # Save the conversation history in S3 as {{variable}}.json
+        s3.put_object(Bucket=os.environ['S3_BUCKET_NAME'], Key=filename, Body=json.dumps(history))
+
+        response_text = f"Conversation history has been saved as {filename}"
+    else:
+        # Add the new message to the history
+        history.append({"role": "user", "content": key1})
+        
+        # Update the request body with the conversation history
+        request_body["messages"] = history
+        
+        # Send the request to the API endpoint
+        headers = {
+            'Authorization': 'Bearer ' + os.environ['API_KEY']
+        }
+        response = requests.post(endpoint_url, json=request_body, headers=headers)
+        
+        # Add the response from the API to the conversation history
+        response_json = response.json()
+        for choice in response_json["choices"]:
+            history.append(choice["message"])
+        
+        # Store the updated history in S3
+        s3.put_object(Bucket=os.environ['S3_BUCKET_NAME'], Key='conversation_history.json', Body=json.dumps(history))
+        
+        response_text = response_json["choices"][0]["message"]["content"]
+
     return {
         "conversation_history": history,
-        "response": response_json
+        "response": {
+            "id": "",
+            "object": "",
+            "created": "",
+            "model": "",
+            "usage": {
+                "prompt_tokens": 0,
+                "completion_tokens": 0,
+                "total_tokens": 0
+            },
+            "choices": [
+                {
+                    "message": {
+                        "role": "assistant",
+                        "content": response_text
+                    },
+                    "finish_reason": "",
+                    "index": 0
+                }
+            ]
+        }
     }
